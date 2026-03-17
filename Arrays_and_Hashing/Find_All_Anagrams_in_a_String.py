cad73454b7eb5aa72bac209bd1a6d006e86d0420
elmouatazti24-gif <el.mouataz_ti24@nusaputra.ac.id>
2026-03-17 08:46:21 +0000

    New Problems have been solved

---
+++ Arrays_and_Hashing/Find_All_Anagrams_in_a_String.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+def examp():
+    s = "cbaebabacd"
+    p = "abc"
+
+    p_count = Counter(p)
+    window = Counter()
+    result = []
+
+    left = 0
+
+    for right in range(len(s)):
+        window[s[right]] += 1
+
+        if right - left + 1 > len(p):
+            window[s[left]] -= 1
+
+            if window[s[left]] == 0:
+                del window[s[left]]
+            left += 1
+
+        if window == p_count:
+            result.append(left)
+
+            
+
+
+    print(result)
+
+examp()
---
+++ Arrays_and_Hashing/Subarray_Sum_Equals_K.py
@@ -0,0 +1,11 @@
+s = [1, 2, 3]
+
+sum = []
+
+k = 3
+
+for i in range(len(s)):
+    total = 0 
+    for j in range(i, len(s)):
+        print(s[j])
+        
