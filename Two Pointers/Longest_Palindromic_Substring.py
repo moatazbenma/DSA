8c0de85fa5828bc150701e2187d32d437ad8b383
elmouatazti24-gif <moatazbanmansour@gmail.com>
2026-04-24 09:44:25 +0100

    Longest Palindromic Substring

--- Two Pointers/Longest_Palindromic_Substring.py
+++ Two Pointers/Longest_Palindromic_Substring.py
@@ -1,28 +1,35 @@
 def examp():
-    s = "racecarxyz"
+    s = "abba"
+    longest = ""
 
+    for i in range(len(s)):
 
-
-
-
-    
-    for i in range(1, len(s) - 1):
+        # odd
         left = i - 1
         right = i + 1
 
-        while left >= 0  and right < len(s) and s[left] == s[right]:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
 
+        candidate1 = s[left+1:right]
 
-        print(s[left + 1:right])
+        if len(candidate1) > len(longest):
+            longest = candidate1
 
+        # even
+        lefta = i
+        righta = i + 1
 
+        while lefta >= 0 and righta < len(s) and s[lefta] == s[righta]:
+            lefta -= 1
+            righta += 1
 
-        
+        candidate2 = s[lefta+1:righta]
 
+        if len(candidate2) > len(longest):
+            longest = candidate2
 
-
+    print(longest)
 
 examp()
-        
