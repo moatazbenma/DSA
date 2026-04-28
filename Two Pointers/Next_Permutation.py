332e597e5f63748f54a71e62b45d5e6ba0625439
elmouatazti24-gif <moatazbanmansour@gmail.com>
2026-04-28 15:38:12 +0100

    next permutation

---
+++ Two Pointers/Next_Permutation.py
@@ -0,0 +1,13 @@
+def examp():
+
+
+    nums = [2,3,1]
+
+    for i in range(len(nums) - 2, -1, -1):
+        if nums[i] < nums[i + 1]:
+            temp = nums[i]
+            
+
+    print(nums)
+
+examp()
