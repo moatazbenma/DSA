ee6b3a68e56e46f9df89a63b4700c62f9ae78e65
elmouatazti24-gif <moatazbanmansour@gmail.com>
2026-04-29 14:19:57 +0100

    Solve Next Permutation

--- Two Pointers/Next_Permutation.py
+++ Two Pointers/Next_Permutation.py
@@ -3,11 +3,25 @@ def examp():
 
     nums = [2,3,1]
 
+   
     for i in range(len(nums) - 2, -1, -1):
-        if nums[i] < nums[i + 1]:
-            temp = nums[i]
-            
+        for j in range(len(nums) - 1, i, -1):
 
-    print(nums)
+            if nums[j] > nums[i]:
+                temp = nums[j]
+                nums[j] = nums[i]
+                nums[i] = temp
+                nums[i+1:] = reversed(nums[i+1:])
+                print(nums)
+                return
+
+            
+                
+    # edge case (no i found)
+    nums.reverse()
+    print(nums)    
+                
+
+
 
 examp()
