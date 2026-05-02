ff4ba279f538afad412d19072e78ce39213bcb47
elmouatazti24-gif <moatazbanmansour@gmail.com>
2026-05-02 10:52:36 +0100

    Solve Find the duplicate number

---
+++ Two Pointers/Find_the_Duplicate_Number.py
@@ -0,0 +1,29 @@
+def examp():
+
+    nums = [3,1,3,4,2]
+
+
+    slow = nums[0]
+    fast = nums[0]
+
+    while True:
+        
+        
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+
+        if slow == fast:
+
+            break
+
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+            
+    print(slow)
+
+    
+
+
+examp() 
