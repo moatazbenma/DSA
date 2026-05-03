1ffb81bb132341846764786e846aec780293adae
elmouatazti24-gif <moatazbanmansour@gmail.com>
2026-05-03 06:25:47 +0100

    Solve 3Sum

--- Two Pointers/3Sum.py
+++ Two Pointers/3Sum.py
@@ -4,21 +4,43 @@ def examp():
 
     nums.sort()
 
-    left = 0
-    right = len(nums) - 1
-    search = 1
+
+
     
     elements = []
 
 
-    for search in range(0, nums - 2):
-        if nums[search] + (nums[left] + nums[right]) == 0:
-            elements.append([nums[search], nums[left], nums[right]])
-            left += 1
-            right -= 1
+    for search in range(len(nums) - 2):
+        left = search + 1
+        right = len(nums) - 1
 
-            
-        search += 1 
+        if search > 0 and nums[search] == nums[search - 1]:
+                continue
+
+        while left < right:
+
+
+            if nums[search] + (nums[left] + nums[right]) < 0:
+                left += 1
+
+            elif nums[search] + (nums[left] + nums[right]) > 0:
+                right -= 1
+
+
+            else:
+
+                triplet = [nums[search], nums[left], nums[right]]
+
+
+                elements.append(triplet)
+                right -= 1 
+                left += 1
+
+                while left < right and nums[left] == nums[left - 1]:
+                    left += 1
+
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
 
 
     print(elements)
